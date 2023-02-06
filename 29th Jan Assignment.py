368a7e0c142fdb5d35acbca439899f81b4295693
rutvikmahant <114412240+rutvikmahant@users.noreply.github.com>
2023-02-06 12:11:31 +0530

    Add files via upload

---
+++ 29th Jan Assignment.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+Que1="Guido van Rossum"
+
+
+# In[12]:
+
+
+Que2="Python is a multi-paraigm language. Object Oriented Programming and structured programming are fully supported,and many of their features support functional programming and aspect-oriented programming."
+
+
+# In[13]:
+
+
+Que3="Yes, Pyhon is case sensitive when dealing with identifiers."
+
+
+# In[16]:
+
+
+Que4=".py is the correct extension of the python file."
+
+
+# In[17]:
+
+
+Que5="Python is interpreted language."
+
+
+# In[18]:
+
+
+Que6="Identation is used to define a block of code in python language. It is required in python to indicate which block of code a statement belongs to."
+
+
+# In[19]:
+
+
+Que7="#(Hash) character is used to give single line comment in python."
+
+
+# In[23]:
+
+
+Que8="The function sys.version can help us to find the version of the python that we are currently working on"
+
+
+# In[25]:
+
+
+Que9="It uses a construct called lambda."
+
+
+# In[26]:
+
+
+Que10="preferred installer program."
+
+
+# In[27]:
+
+
+Que11="print(),type(),input(),max(),len(),pow(),etc..."
+
+
+# In[28]:
+
+
+Que12="79 is the maximum possible length of an identifier in python."
+
+
+# In[29]:
+
+
+Que13="Python is arguably one of the most widely used programming language in the world."
+"It can be used in: Desktop Applications, Mobile Applications, Database-driven softwares, Operating Systems,Data Science and Data Analysis, etc... "
+
+
+# In[30]:
+
+
+Que14="Memory management in Python involves a private heap containing all Python objects and data structures.The management of this private heap is ensured internally by the Python memory manager."
+
+
+# In[31]:
+
+
+Que15="1.Install Python from python.org. 2.Manually add Python to Windows Path."
+
+
+# In[32]:
+
+
+Que16="Identation refers to the spaces at the beginning of a code line.Where in other programming languages the identation in code is for readibility only,the identation in Python is very important.Python uses identation to indicate a block of code."
+
+
+# In[ ]:
+
+
+
+
